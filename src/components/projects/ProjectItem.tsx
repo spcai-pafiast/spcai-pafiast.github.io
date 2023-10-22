e2fa99c49b9509242be2d3684b48d5d035bd989e
Izzet Yildirim <iyildirim@hawk.iit.edu>
2023-10-22 16:56:55 -0500

    Margin added to project card.

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -4,7 +4,7 @@ import { Project } from "@site/src/types";
 
 export default function ProjectItem({ link, shortDescription, title }: Project) {
   return (
-    <div className="col col--4">
+    <div className="col col--4 margin-bottom--lg">
       <div className="card text--center">
         <div className="card__header">
           <h3>{title}</h3>
