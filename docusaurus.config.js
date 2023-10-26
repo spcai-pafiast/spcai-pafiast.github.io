823eb8c905602939fbf6c1cdadcfc09e990737e1
Izzet Yildirim <iyildirim@hawk.iit.edu>
2023-10-26 17:36:27 -0500

    Production url fixed.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://grc-iit.github.io/",
+  url: "https://grc.iit.edu/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
