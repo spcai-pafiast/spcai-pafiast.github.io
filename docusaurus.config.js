480164b5bb38160ab2ba05db75c515df4fe56eac
Izzet Yildirim <iyildirim@hawk.iit.edu>
2023-10-25 20:50:26 -0500

    Google Analytics tracking ID added.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,6 +61,9 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-9SXDPLSLB2",
+        },
       }),
     ],
   ],
