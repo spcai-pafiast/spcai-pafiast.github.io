a89bb8bbe1cd283c8ac43d3ca9358d6e74c97e03
Izzet Yildirim <iyildirim@hawk.iit.edu>
2024-01-15 18:24:32 -0600

    Docusaurus upgraded.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer");
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 const GNOSIS_SHORT = "GRC";
 const GNOSIS_TITLE = "Gnosis Research Center";
@@ -273,8 +274,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} ${GNOSIS_TITLE} (${GNOSIS_SHORT}).`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
     }),
 };
