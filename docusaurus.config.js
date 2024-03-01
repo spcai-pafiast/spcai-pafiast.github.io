302487cfc446b3ae9531ba1434a503ef1a11b083
emesk <sohail.ceme@gmail.com>
2024-03-01 12:27:51 +0500

    third run including build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "grc-iit", // Usually your GitHub org/user name.
+  organizationName: "spcai-pafiast", // Usually your GitHub org/user name.
   projectName: "website", // Usually your repo name.
 
   onBrokenLinks: "throw",
